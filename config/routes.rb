80787952b1c496c22713a56360c93ce0a53f40bc
Juan Tamayo <juan@crestandco.com>
2014-12-31 10:52:55 -0500

    Add a full_title_helper

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Whosmilk2::Application.routes.draw do
   get "static_pages/help"
 
   get "static_pages/about"
+
+  get "static_pages/contact_us"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
