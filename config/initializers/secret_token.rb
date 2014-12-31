f5953ba0cc55b703009b1822c5901659a9d55fb0
Juan Tamayo <juan@crestandco.com>
2014-12-31 02:10:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whosmilk2::Application.config.secret_token = 'e145382ea926c2dd8af5aa78d099c56a47a9604c93fbdde8faa08051ca9a1a1c82d9bb22baf6c5ddbe14f7634d15d3f8c78404d49082d8e213ec983ae78df1af'
